d12e9b38da155d468b01b482993155b5de0ba919
Volodymyr Baisa <biosvova@gmail.com>
2020-12-13 22:05:22 -0500

    db.js add checkDatabase() and saveRecord()

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -20,9 +20,58 @@ openRequest.onupgradeneeded = function (e) {
 };
 openRequest.onsuccess = function (e) {
     db = e.target.result;
-
-    // if(navigator.onLine) addItem();
+    if (navigator.onLine) checkDatabase();
 };
 openRequest.onerror = function (e) {
     console.dir(e);
 };
+
+function saveRecord(record) {
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+
+    // add record to your store with add method.
+    store.add(record);
+}
+
+function checkDatabase() {
+    // open a transaction on your pending db
+    const transaction = db.transaction(["pending"], "readwrite");
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+    // get all records from store and set to a variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then(() => {
+                    // if successful, open a transaction on your pending db
+                    const transaction = db.transaction(
+                        ["pending"],
+                        "readwrite"
+                    );
+
+                    // access your pending object store
+                    const store = transaction.objectStore("pending");
+
+                    // clear all items in your store
+                    store.clear();
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
